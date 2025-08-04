63b59ae93f2346b4e960fa2e68e0d69e8e38bd65
SpencerLiege <nseabasi269@gmail.com>
2025-08-04 15:25:02 +0100

    entry points

--- src/contract.rs
+++ src/contract.rs
@@ -1,41 +1,57 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
 // use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
-/*
+use crate::instantiate::instantiate as contract_instance;
+use crate::execute::execute as contract_executions;
+use crate::query::query as contract_queries;
+
+
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:zigstake";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: InstantiateMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+
+    // Set contract version
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    // Initialize the contract
+    contract_instance(deps, env, info, msg).map_err(ContractError::Std)
+
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    contract_executions(deps, info, env, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    unimplemented!()
+pub fn query(
+    deps: Deps, 
+    env: Env, 
+    msg: QueryMsg
+) -> StdResult<Binary> {
+    contract_queries(deps, env, msg)
 }
 
+// Contract test will go here and will be update
 #[cfg(test)]
 mod tests {}
